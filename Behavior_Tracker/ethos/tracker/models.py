33cf85de018a3d6faad64f6e22a925678bbc5301
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2026-05-06 12:52:20 +0800

    feat: Define Log Entry model

--- Behavior_Tracker/ethos/tracker/models.py
+++ Behavior_Tracker/ethos/tracker/models.py
@@ -1,9 +1,30 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Behavior(models.Model):
     title = models.TextField(max_length=20)
     description = models.TextField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 class LogEntry(models.Model):
-    pass
+    date = models.DateTimeField(default=timezone.now)
+    behavior = models.ForeignKey(
+        Behavior,
+        on_delete=models.CASCADE,
+        related_name='logs'
+    )
+    quality = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5)
+        ],
+        help_text="Rate how it went from 0 to 5."
+    )
+    special_note = models.TextField(max_length=250)
+    
+    def __str__(self):
+        return f"{self.behavior.name} on {self.date}"
