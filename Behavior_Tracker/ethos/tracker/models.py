61a47a6b8c1975770e381251428ff46a68fd13e5
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2026-05-07 04:51:46 +0800

    feat: Add validation logic for custom metrics

--- Behavior_Tracker/ethos/tracker/models.py
+++ Behavior_Tracker/ethos/tracker/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Behavior(models.Model):
@@ -31,6 +32,36 @@ class LogEntry(models.Model):
     special_note = models.TextField(max_length=250)
 
     custom_metrics = models.JSONField(default=dict, blank=True)
-    
+
+    def clean(self):
+        # From what I've learned, the reason why super.clean() should be called here
+        # is for future proofing because (1) the devs of Django might change the currently
+        # empty clean method and (2) because I might create a Mixin class that LogEntry could
+        # inherit from someday.
+        super.clean()
+
+        expected_metrics = self.behavior.expected_metrics
+        actual_metrics = self.custom_metrics
+
+        # Verify if all expected metrics defined under specific Behavior are present 
+        for key in expected_metrics:
+            if key not in actual_metrics:
+                raise ValidationError(f"Missing required metric '{key}' for {self.behavior.title}")
+            
+        # Verify that there are no rogue metrics under actual metrics
+        for key in actual_metrics:
+            if key not in expected_metrics:
+                raise ValidationError(f"An invalid metric was added: {key}")
+            
+        # Verify that the provided metrics have the correct type
+        for key, expected_type in actual_metrics.items():
+            value = actual_metrics[key]
+
+            if expected_type == 'integer' and not isinstance(key, int):
+                raise ValidationError(f"{key} must be of type 'int', instead got: {type(value).__name__}")
+
+            if expected_type == "string" and not isinstance(key, str):
+                raise ValidationError(f"{key} must be of type 'str', instead got: {type(value).__name__}")
+
     def __str__(self):
         return f"{self.behavior.title} on {self.date}"
