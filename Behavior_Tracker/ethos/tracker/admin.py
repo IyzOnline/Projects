060bdb92d551e6fb086e9eb115fafc3bfac9e53c
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2026-05-09 07:38:41 +0800

    feat: Register models to admin

--- Behavior_Tracker/ethos/tracker/admin.py
+++ Behavior_Tracker/ethos/tracker/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Behavior, LogEntry
+
 # Register your models here.
+@admin.register(Behavior)
+class BehaviorAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['date', 'behavior', 'quality']
