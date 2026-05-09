6dc5c2f8cb500eb51e626dcbf8f9def02e3a786a
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2026-05-09 15:18:10 +0800

    feat: Add home as default path to tracker url

--- Behavior_Tracker/ethos/tracker/urls.py
+++ Behavior_Tracker/ethos/tracker/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'tracker'
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('<int:behavior_id>/', views.behavior_dashboard, name='behavior_dashboard'),
 ]
