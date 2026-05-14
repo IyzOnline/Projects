34a05055616fe96b2787a31f3ab68590de2517b0
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2026-05-14 18:13:39 +0800

    feat: Register behavior creation method as create path

--- Behavior_Tracker/ethos/tracker/urls.py
+++ Behavior_Tracker/ethos/tracker/urls.py
@@ -6,5 +6,6 @@ app_name = 'tracker'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create/', views.create_behavior, name='create_behavior'),
     path('<int:behavior_id>/', views.behavior_dashboard, name='behavior_dashboard'),
 ]
